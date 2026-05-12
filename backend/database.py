eb93894bc1ee86469a1632a91144d895bf89ea10
Rishabh bambani <bhambanirishabh@gmail.com>
2026-05-12 15:03:00 +0530

    fix: use certifi CA bundle for Atlas TLS on Render

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,5 @@
 import os
+import certifi
 from pymongo import AsyncMongoClient
 from dotenv import load_dotenv
 
@@ -7,7 +8,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "pyide")
 
-client = AsyncMongoClient(MONGO_URI)
+client = AsyncMongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client[DB_NAME]
 
 
