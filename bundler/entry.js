58bf0a5dce0a4e9068cb2e5e1e5e40ec172baf4d
Rishabh <bhambanirishabh@gmail.com>
2026-05-17 15:14:42 +0530

    chore: remove bundler/ and codemirror-bundle.js

--- bundler/entry.js
+++
@@ -1,6 +0,0 @@
-export { EditorState, StateEffect, StateField, RangeSetBuilder } from '@codemirror/state';
-export { EditorView, keymap, Decoration, WidgetType } from '@codemirror/view';
-export { indentWithTab } from '@codemirror/commands';
-export { basicSetup } from 'codemirror';
-export { python } from '@codemirror/lang-python';
-export { oneDark } from '@codemirror/theme-one-dark';
